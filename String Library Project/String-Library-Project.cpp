ab4da2bd61c74e76dcf0016e15e3c9ac918bb51e
Pro_Mr_SiR <mgoihtahmumb@gmail.com>
2026-04-28 23:11:14 +0300

    Create String-Library-Project.cpp

---
+++ String Library Project/String-Library-Project.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "clsString.h"
+
+using namespace std;
+
+int main()
+
+{
+    clsString String1;
+
+    String1.SetValue("Mohammed");
+
+    clsString String2("MR.SiR");
+
+    return 0;
+};
